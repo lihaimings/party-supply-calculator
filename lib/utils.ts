b207b8d58f953d9713143ffd0dbf4a869469e059
LJli <1044257790@qq.com>
2026-01-02 17:01:05 +0800

    Add lib/utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,14 @@
+export function cn(...inputs: (string | undefined | null | false)[]) {
+  return inputs.filter(Boolean).join(' ')
+}
+
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount)
+}
+
+export function formatNumber(num: number): string {
+  return new Intl.NumberFormat('en-US').format(num)
+}
